5e29e116aabecc2d5e14bb37820cea3b30984113
rosselton <rebecca.osselton@ncl.ac.uk>
2022-04-01 13:31:26 +0100

    Add file to auto-generate Slack notifcation

--- src/api/submission/content-types/submission/lifecycles.js
+++ src/api/submission/content-types/submission/lifecycles.js
@@ -2,17 +2,23 @@
 const fetch = require("node-fetch");
 
 module.exports = {
-      async beforeCreate(data) {
-        const body = {
-          text: "NAS submission created : "
-        };
-        const response = await fetch(
-          `https://hooks.slack.com/services/`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body),
-          }
-        );
-    },
+
+    async afterCreate(event) {
+
+      const { result, params } = event;
+      const title = event.params.data.title;
+      const uuid = event.params.data.uuid;
+
+      const body = {
+        text: "NAS submission created - Title : " + title + " UUID : " + uuid
+      };
+      const response = await fetch(
+        `https://hooks.slack.com/services/`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+    }
 }
