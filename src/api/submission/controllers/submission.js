6d807a93e327a777871b337b93e46b9b6248a590
Mark Turner <mail@markdturner.co.uk>
2022-05-03 10:00:32 +0100

    Update submission.js
    Reduced max wall clock time from 24 hours to 1 hour

--- src/api/submission/controllers/submission.js
+++ src/api/submission/controllers/submission.js
@@ -146,7 +146,7 @@ module.exports = createCoreController('api::submission.submission', ({strapi}) =
                 },
                 constraints: {
                     maxTaskRetryCount: 3,
-                    maxWallClockTime: 'P1D'
+                    maxWallClockTime: 'P1H'
                 },
                 outputFiles: [
                     {
